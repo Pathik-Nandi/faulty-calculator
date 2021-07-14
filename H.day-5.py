5aa830992c9783c4c09d420463060d88e56fd743
Pathik-Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2021-07-14 09:14:21 +0530

    Update H.day-5.py

--- H.day-5.py
+++ H.day-5.py
@@ -1,39 +1,6 @@
-#if and elif:
 
 '''
-var1=65
-var2=int(input("Enter a number: "))
-if var1>var2:
-    print("lesser")
-elif var2==65:
-    print("equal")
-else:
-    print("grater")
-'''
-'''
-a=5
-age=int(input("Enter your age: "))
-
-if age<18:
-   # if age<18:
-        print("you are eigible")
-elif age==18:
-    print("not decied yet")
-else:
-    print("you are not eligible for typeing")
-'''
-"""
-list=[2,3,4,5]
-print(5 in list)
-print(15 not in list)
-if 5 in list:
-    print("yes 5 in the list")
-else:
-    print(" not in a list")
-"""
-'''
-**Designe a calculator which will correctkly slove all the problem
-except the follwoing ones:
+**Designe a calculator which will correctkly slove all the problem except the follwoing ones:
 45*3=555, 56+9=77,56/6=4
 '''
 inp1 = input("operator")
